487fb218c1e866b2675480f15081efe50a52c41d
xIKurenai <312136533+xIKurenai@users.noreply.github.com>
2026-08-18 21:47:50 +0200

    Add fastfetch alias

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,2 +1,49 @@
+# ==========================================
+# Navigation
+# ==========================================
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+alias dots='cd ~/philly-dotfiles'
+
+
+# ==========================================
+# Files
+# ==========================================
+
+alias ls='eza -al --color=always --group-directories-first --icons=always'
+alias ll='eza -l --color=always --group-directories-first --icons=always'
+alias la='eza -a --color=always --group-directories-first --icons=always'
+alias lt='eza -aT --color=always --group-directories-first --icons=always'
+
 alias cls='clear'
+alias ff='fastfetch'
+
+
+# ==========================================
+# System
+# ==========================================
+
 alias top='btop'
+alias ports='ss -tulpn'
+
+alias update='sudo cachyos-rate-mirrors && sudo pacman -Syu'
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+
+alias fixpacman='sudo rm /var/lib/pacman/db.lck'
+alias mirror='sudo cachyos-rate-mirrors'
+
+
+# ==========================================
+# Git
+# ==========================================
+
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gpl='git pull'
+
+alias glog='git log --oneline --graph --decorate --all'
